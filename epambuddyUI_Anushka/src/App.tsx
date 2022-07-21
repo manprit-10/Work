6528dbe25a15c64f2ad3d6163833b1c233ddce95
Manpritsingh Saluja <manpritsingh_saluja@epam.com>
2022-07-21 15:06:33 +0530

    New Files

--- epambuddyUI_Anushka/src/App.tsx
+++ epambuddyUI_Anushka/src/App.tsx
@@ -1,18 +1,13 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link
-} from "react-router-dom";
-import Home from './pages/home/Home';
-import SearchResults from './pages/search/SearchResults';
-import EmployeeBenefits from './pages/tabs/employee/benefits/EmployeeBenefits';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/home/Home";
+import SearchResults from "./pages/search/SearchResults";
+import EmployeeBenefits from "./pages/tabs/employee/benefits/EmployeeBenefits";
 import NavBar from "./components/navbar/NavBar";
 
-import './App.css';
-import '@epam/uui-components/styles.css';
+import "./App.css";
+import "@epam/uui-components/styles.css";
 import SideBar from "./components/sidebar/SideBar";
-
+import { MainRoutes } from "./pages/tabs/employee/benefits/components/Routes";
 
 function App() {
   return (
@@ -20,15 +15,17 @@ function App() {
       <Router>
         <NavBar />
         <div className="wrapper">
-          <SideBar/>
+          <SideBar />
           <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/search" element={ <SearchResults/>} />
-          <Route path="/employee/benefits" element={<EmployeeBenefits />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/employee/benefits" element={<EmployeeBenefits />} />
           </Routes>
-        <div className="extra-space"></div>
+          <MainRoutes />
+
+          <div className="extra-space"></div>
         </div>
-        </Router>
+      </Router>
     </div>
   );
 }
---
+++ epambuddyUI_Anushka/src/pages/tabs/employee/benefits/Accordian.js
@@ -0,0 +1,44 @@
+import "./Accordian.css";
+
+const Accordian = (props) => {
+  const getContent = function (data) {
+    return Object.entries(data).map(function (item) {
+      return (
+        <li key={item}>
+          <p className="questions">
+            {item[0]} : {item[1][item[0]]}
+          </p>
+          <p>A : {item[1]["A"]}</p>
+        </li>
+      );
+    });
+  };
+
+  const clickHandler = function () {
+    props.onClick(props.title);
+  };
+  return (
+    <div className="wrap" onClick={clickHandler}>
+      <div className="accordian">
+        <div className="item">
+          <div className="title">
+            <span>{props.isActive === props.title ? "-" : "+"}</span>
+            <h2>{props.title}</h2>
+          </div>
+          <div
+            className={
+              // props.isActive === props.title ? "content show" : "content"
+              "content show"
+            }
+          >
+            {/* <div className="content show"> */}
+            <hr></hr>
+            <ul className="questions-list">{getContent(props.questions)}</ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordian;
