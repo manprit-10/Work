b20e0a18bcc52387efe78b4ce63d42e56f2bc930
Paras Rawat <parasrawat937@gmail.com>
2022-07-25 10:15:27 +0530

    css changes

--- epambuddyUI_Anushka/src/pages/tabs/employee/benefits/EmployeeBenefits.tsx
+++ epambuddyUI_Anushka/src/pages/tabs/employee/benefits/EmployeeBenefits.tsx
@@ -4,25 +4,38 @@ import { Link, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 export default () => {
-  const [isActive, setIsActive] = useState(null);
+  const [isActive, setIsActive] = useState("");
+  const clickHandler = function (e: any) {
+    setIsActive(e.target.textContent.toLowerCase().split(" ")[0]);
+    console.log(isActive);
+  };
 
   return (
     <div>
       <div className="heading">
         <ul>
-          <li className="link ">
-            <Link to="/employee/benefits/leaves">Leaves</Link>
+          <li className={`link ${isActive === "leaves" && "active"}`}>
+            <Link to="/employee/benefits/leaves" onClick={clickHandler}>
+              Leaves
+            </Link>
           </li>
-          <li className="link">
-            <Link to="/employee/benefits/medicalbenefits">
+          <li className={`link ${isActive === "medical" && "active"}`}>
+            <Link
+              to="/employee/benefits/medicalbenefits"
+              onClick={clickHandler}
+            >
               Medical Benefits
             </Link>
           </li>
-          <li className="link">
-            <Link to="/employee/benefits/recognition">Recognition</Link>
+          <li className={`link ${isActive === "recognition" && "active"}`}>
+            <Link to="/employee/benefits/recognition" onClick={clickHandler}>
+              Recognition
+            </Link>
           </li>
-          <li className="link">
-            <Link to="/employee/benefits/taxbenefits">Tax Benefits</Link>
+          <li className={`link ${isActive === "tax" && "active"}`}>
+            <Link to="/employee/benefits/taxbenefits" onClick={clickHandler}>
+              Tax Benefits
+            </Link>
           </li>
         </ul>
       </div>
